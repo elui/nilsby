9db9018c8fb862148f5bb0d0000561297df6b763
Frank Huang <me@nongraphical.com>
2012-01-21 14:24:30 -0800

    Using view_config

--- nilsby/__init__.py
+++ nilsby/__init__.py
@@ -11,8 +11,6 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'nilsby:static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_view('nilsby.views.my_view',
-                    route_name='home',
-                    renderer='templates/mytemplate.pt')
+    config.scan()
     return config.make_wsgi_app()
 
--- nilsby/views.py
+++ nilsby/views.py
@@ -1,6 +1,9 @@
+from pyramid.view import view_config
+
 from nilsby.models import DBSession
 from nilsby.models import MyModel
 
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     dbsession = DBSession()
     root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()

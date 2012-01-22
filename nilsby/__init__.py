035be5e00781abf5b8123abca3125ae2663798a5
Stephanie H. Chang <chang.steph.h@gmail.com>
2012-01-21 17:20:17 -0800

    Added session factory (Frank)

--- nilsby/__init__.py
+++ nilsby/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
 from sqlalchemy import engine_from_config
 
 from nilsby.models import initialize_sql
@@ -8,7 +9,8 @@ def main(global_config, **settings):
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     initialize_sql(engine)
-    config = Configurator(settings=settings)
+    session_factory = UnencryptedCookieSessionFactoryConfig('nilsbysite')
+    config = Configurator(settings=settings, session_factory=session_factory)
     config.add_static_view('static', 'nilsby:static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('forum_index', '/forum')

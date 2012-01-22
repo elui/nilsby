f11eae53cbd7482bf5c8fb92edecca60cc4ffccd
Stephanie H. Chang <chang.steph.h@gmail.com>
2012-01-21 19:28:12 -0800

    made user view RESTful

--- nilsby/templates/user_view.mako
+++ nilsby/templates/user_view.mako
@@ -1,10 +1,8 @@
 <%inherit file="layout.mako"/>
 
 <%block name="content">
-    %for p in people:
-    <p>${p.username}</p>
-    <p>Name: ${p.realname}</p>
-    %endfor
+    <p>${person.username}</p>
+    <p>Name: ${person.realname}</p>
 
     <a href="${request.route_url('forum_post')}">Back to User Index</a>
 </%block>
--- nilsby/user_views.py
+++ nilsby/user_views.py
@@ -5,12 +5,18 @@ from nilsby.models import *
 
 @view_config(route_name='user_index', renderer='user_index.mako')
 def user_index(request):
-    dbsession = DBSession()
-    people = dbsession.query(Person)
-    return {'people': people, 'project':'nilsby'}
+    db = DBSession()
+    people = db.query(Person)
+    return {'people': people}
     
 @view_config(route_name='user_view', renderer='user_view.mako')
 def user_view(request):
-    dbsession = DBSession()
-    people = dbsession.query(Person)
-    return {'people': people, 'project':'nilsby'}
+    db = DBSession()
+    person = db.query(Person).filter(Person.id==request.matchdict['id']).first()
+    return {'person': person}
+
+@view_config(route_name='user_view', renderer='user_view.mako')
+    def user_view(request):
+    db = DBSession()
+    person = db.query(Person).filter(Person.id==request.matchdict['id']).first()
+    return {'person': person}
